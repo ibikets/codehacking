79c32a0d8397187bed6270577675a34a0f338ca3
Ib <Ibikets@THREEMs-iMac.local>
2018-08-23 15:18:05 +0100

    Commit at css update and admin master page setup

---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.admin')
+
+@section('title')
+
+
+
+@endsection
+
+
+@section('content')
+
+    <h1>Admin Users Page</h1>
+
+@endsection
+
+
+@section('footer')
+
+
+
+@endsection
